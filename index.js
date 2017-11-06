b3b3003ae6e0a38592b401070f5d2a40dce127d3
Denis Iogansen <pofigizm@gmail.com>
2017-11-06 13:29:32 +0300

    some refactor

--- index.js
+++ index.js
@@ -1,59 +1,39 @@
-const plugins = [
-  require.resolve('babel-plugin-universal-import'),
-
-  require.resolve('babel-plugin-syntax-object-rest-spread'),
-  require.resolve('babel-plugin-syntax-export-extensions'),
-  require.resolve('babel-plugin-syntax-class-properties'),
-
-  [require.resolve('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
-  require.resolve('babel-plugin-transform-export-extensions'),
-  require.resolve('babel-plugin-transform-class-properties'),
-
-  [require.resolve('babel-plugin-transform-react-jsx'), { useBuiltIns: true }],
-]
-
 const env = process.env.BABEL_ENV || process.env.NODE_ENV
+const dev = env !== 'production'
 
-if (!env) throw new Error('Need to setup BABEL_ENV or NODE_ENV')
-
-if (env === 'development' || env === 'test') {
-  plugins.push.apply(plugins, [
-    require.resolve('babel-plugin-transform-react-jsx-source'),
-    require.resolve('babel-plugin-transform-react-jsx-self'),
-  ])
-}
-
-if (env === 'test' || env === 'server') {
-  module.exports = {
-    presets: [
-      [
-        require('babel-preset-env').default,
-        {
-          targets: { node: 'current' },
-        },
-      ],
-      require.resolve('babel-preset-react'),
-    ],
-    plugins: plugins.concat([
-      require.resolve('babel-plugin-dynamic-import-node'),
-    ]),
+module.exports = function (context, opts) {
+  const targets = opts && opts.targets
+  const node = {
+    targets: { node: 'current' },
   }
-} else {
-  module.exports = {
+  const browser = {
+    targets: targets,
+    useBuiltIns: false,
+    modules: false,
+    loose: true,
+  }
+
+  return {
     presets: [
-      [
-        require.resolve('babel-preset-env'),
-        {
-          targets: { chrome: 60 },
-          useBuiltIns: false,
-          modules: false,
-          loose: true,
-        },
-      ],
+      [require.resolve('babel-preset-env'), targets ? browser : node],
       require.resolve('babel-preset-react'),
     ],
-    plugins: plugins.concat([
-      require.resolve('babel-plugin-syntax-dynamic-import'),
-    ]),
+    plugins: [
+      require.resolve('babel-plugin-universal-import'),
+      targets && require.resolve('babel-plugin-syntax-dynamic-import'),
+      !targets && require.resolve('babel-plugin-dynamic-import-node'),
+
+      require.resolve('babel-plugin-syntax-object-rest-spread'),
+      require.resolve('babel-plugin-syntax-export-extensions'),
+      require.resolve('babel-plugin-syntax-class-properties'),
+
+      [require.resolve('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }],
+      require.resolve('babel-plugin-transform-export-extensions'),
+      require.resolve('babel-plugin-transform-class-properties'),
+
+      [require.resolve('babel-plugin-transform-react-jsx'), { useBuiltIns: true }],
+      dev && require.resolve('babel-plugin-transform-react-jsx-source'),
+      dev && require.resolve('babel-plugin-transform-react-jsx-self'),
+    ].filter(Boolean),
   }
 }
